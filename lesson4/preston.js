614c34141617a58d618cf8ca6a9eb706afbb0b01
Jstaker <sta15009@byui.edu>
2021-10-08 11:59:40 -0600

    buncha change back

--- lesson4/preston.js
+++ lesson4/preston.js
@@ -1,3 +1,3 @@
 function toggleMenu() {
-    document.getElementById("weather-nav")[0].classList.toggle("responsive");
+    document.getElementsByClassName("weather-nav")[0].classList.toggle("responsive");
 }
