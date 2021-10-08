ec2cb24fc669b845e8d2d5c3817ada1a7cdf0463
Jstaker <sta15009@byui.edu>
2021-10-08 11:57:09 -0600

    #

--- lesson4/preston.js
+++ lesson4/preston.js
@@ -1,3 +1,3 @@
 function toggleMenu() {
-    document.getElementsByClassName("weather-nav")[0].classList.toggle("responsive");
+    document.getElementById("weather-nav")[0].classList.toggle("responsive");
 }
