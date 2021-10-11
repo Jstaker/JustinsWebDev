386d7c80384df2103b212f2821ebd1897fd3f696
Jstaker <sta15009@byui.edu>
2021-10-11 14:11:23 -0600

    trying some more js

--- lesson4/preston.js
+++ lesson4/preston.js
@@ -1,26 +1,36 @@
 //toggle menu on mobile view
-function toggleMenu() {
-    document.getElementsByClassName("weather-nav")[0].classList.toggle("responsive");
-}
-
-
-//last updated js
-var day = new Array(
-    "Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
-);
-
-var month = new Array(
-    "January","February","March","April","May","June","July","August","Sepetember","October","November","December"
-);
-
+var weekday = new Array(
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday",
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+    );
+var months = new Array(
+    "January", 
+    "February", 
+    "March",
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August", 
+    "September",
+    "October", 
+    "November", 
+    "December"
+    );
 const year = new Date().getFullYear();
-const lastUpdated = new Date(document.lastUpdated);
-var x = lastUpdated.getDay();
-var x = day[x];
-var y = lastUpdated.getMonth();
-var y = month[y];
+const lastModified = new Date(document.lastModified);
+var day = lastModified.getDay();
+var day = weekday[day];
+var month = lastModified.getMonth();
+var month = months[month];
 
-var lastUpdatedFormat= x + ", " + lastUpdated.getDate() + " " + y + " " + lastUpdated.getFullYear();
+var lastModifiedFormatted = day + ', ' + lastModified.getDate() + ' ' + month + " " + lastModified.getFullYear();
 
-//output
-document.getElementById("displayDate").textContent=lastUpdatedFormat;
+// Display copyright year and date last modified to footer of HTML document.
+document.getElementById("dateLastModified").textContent = lastModifiedFormatted;
+document.getElementById("copyrightYear").textContent = year;
