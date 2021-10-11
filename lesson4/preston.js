f555f58f9cc3661044bc1b069a427ab830f94d80
Jstaker <sta15009@byui.edu>
2021-10-11 14:16:52 -0600

    new changes

--- lesson4/preston.js
+++ lesson4/preston.js
@@ -1,36 +1,20 @@
 //toggle menu on mobile view
-var weekday = new Array(
-    "Sunday", 
-    "Monday", 
-    "Tuesday", 
-    "Wednesday",
-    "Thursday", 
-    "Friday", 
-    "Saturday"
+var day = new Array(
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
     );
-var months = new Array(
-    "January", 
-    "February", 
-    "March",
-    "April", 
-    "May", 
-    "June", 
-    "July", 
-    "August", 
-    "September",
-    "October", 
-    "November", 
-    "December"
+var month = new Array(
+    "January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"
     );
-const year = new Date().getFullYear();
-const lastModified = new Date(document.lastModified);
-var day = lastModified.getDay();
-var day = weekday[day];
-var month = lastModified.getMonth();
-var month = months[month];
 
-var lastModifiedFormatted = day + ', ' + lastModified.getDate() + ' ' + month + " " + lastModified.getFullYear();
+const year = new Date().getFullYear();
+const lastUpdated = new Date(document.lastUpdated);
+var x = lastUpdated.getDay();
+var x = day[x];
+var y = lastUpdated.getMonth();
+var y = month[y];
+
+var lastUpdatedFormat = x + ', ' + lastUpdated.getDate() + ' ' + y + " " + lastUpdated.getFullYear();
 
 // Display copyright year and date last modified to footer of HTML document.
-document.getElementById("dateLastModified").textContent = lastModifiedFormatted;
+document.getElementById("dateFormat").textContent = lastUpdatedFormat;
 document.getElementById("copyrightYear").textContent = year;
