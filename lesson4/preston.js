2e0f40e3953f376311280691b9d39b190e7f86f7
Jstaker <sta15009@byui.edu>
2021-10-11 13:54:28 -0600

    more js stuffs

--- lesson4/preston.js
+++ lesson4/preston.js
@@ -1,3 +1,24 @@
 function toggleMenu() {
     document.getElementsByClassName("weather-nav")[0].classList.toggle("responsive");
 }
+
+
+//date
+var day = new Array[
+    "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
+];
+
+var month = new Array[
+    "January","February","March","April","May","June","July","August","Sepetember","October","November","December"
+];
+
+const year = new Date().getFullYear();
+const lastUpdated = new Date(document.lastUpdated);
+var x = lastUpdated.getDay();
+var x = day[x];
+var y = lastUpdated.getMonth();
+var y = month[y];
+
+var lastUpdatedFormat=x+", "+lastUpdated.getDate()+", "+y+" "+lastUpdated.getFullYear();
+
+document.getElementById("toDate").textContent=lastUpdatedFormat;
