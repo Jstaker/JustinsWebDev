b2776641c8e53c288db0a78dd9297063de8e429d
Jstaker <sta15009@byui.edu>
2021-10-13 14:01:31 -0600

    looky here

--- lesson4/preston.js
+++ lesson4/preston.js
@@ -23,7 +23,7 @@ var lastModifiedFormatted = day + ', ' +  month + " " + lastModified.getDate() +
 document.getElementById("dateLastModified").textContent = lastModifiedFormatted;
 
 //display pancake day on saturday
-if (weekday[day] == "Wednesday"){
+if (weekday[day] == "Friday"){
     document.getElementById("pancakes").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
     document.getElementById("pancakes").style.display = "block";
 }else{
