f3268296eec55f903c4264bc8b19b78d33e0eaee
Jstaker <sta15009@byui.edu>
2021-10-15 16:29:53 -0600

    display block

--- lesson4/preston.js
+++ lesson4/preston.js
@@ -26,6 +26,7 @@ document.getElementById("dateLastModified").textContent = lastModifiedFormatted;
 //display pancake day on saturday
 if (weekday[day] == "Friday"){
     document.getElementById("pancakes").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    document.getElementById("pancakes").style.display = "block";
 }else{
     document.getElementById("pancakes").style.display = "none";
 }
